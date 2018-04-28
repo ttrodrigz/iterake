6cec26886a7c7ba71f1a04a7dce0d6b541232f04
ttrodrigz <ttrodrigz@gmail.com>
2018-04-28 11:36:50 -0400

    sum.1 argument to wgt_cat()

--- 02-test-code/wgt_cat.R
+++ 02-test-code/wgt_cat.R
@@ -1,4 +1,4 @@
-wgt_cat <- function(name, value, targ.prop) {
+wgt_cat <- function(name, value, targ.prop, sum.1 = FALSE) {
     
     if (!is.character(name) || length(name) != 1) {
         stop("name must be a character vector with one element.")
@@ -12,8 +12,12 @@ wgt_cat <- function(name, value, targ.prop) {
         stop("name must be an non-empty character string.")
     }
     
+    if (isTRUE(sum.1)) {
+        targ.prop <- targ.prop / sum(targ.prop)
+    }
+    
     if (sum(targ.prop) != 1) {
-        stop("Target proportions must sum to 1.")
+        stop("Target proportions must sum to 1, if an issue of rounding set sum.1 = TRUE.")
     }
     
     out <- list(tibble(wgt_cat = name,
