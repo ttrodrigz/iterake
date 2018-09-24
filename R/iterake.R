294cd0def2ed23d3127a7b8cf30669fddbd648b0
ttrodrigz <ttrodrigz@gmail.com>
2018-09-24 15:43:48 -0400

    Bunch o' stuff
    Added error check for duplicate weight names. Simplified error checking in general. wgt_names and wgt_cats were both being used to do the same thing, condensed that. Changed some argument names to be more expressive and consistent (I hope). Some changes to the documentation for iterake as well.

--- R/iterake.R
+++ R/iterake.R
@@ -1,34 +1,39 @@
-#' Iterative rim weighting procedure
+#' Iterative raking procedure
 #' 
-#' This function creates a weight variable that satisfies the targets described from the output of \code{wgt_design()}.
-#' It then appends the weight variable to the data frame used to create the weights.
+#' This function creates row-level weights using an iterative raking algorithm based
+#' on targets from a known population (established with \code{universe()}).
+#' The weights are appended as a new column in the data. If \code{iterake()} 
+#' converges, the weighted marginal proportions of the sample will match those
+#' set in \code{universe()}. Summary statistics of the weighting procedure are 
+#' presented by default.
 #' 
-#' @param df Data frame containing data you intend to weight.
-#' @param design Data frame containing data you intend to weight.
-#' @param wgt.name Character name for created weight variable, optional.
-#' @param wgt.lim Numeric value created weights cannot exceed, optional.
-#' @param threshold Numeric value specifying minimum summed difference between target and weighted proportions, optional.
-#' @param max.iter Numeric value capping number of iterations for the procedure, optional.
-#' @param stuck.limit Numeric value capping the number of times summed differences between target and weighted
-#' proportions can oscillate between increasing and decreasing, optional.
-#' @param N Numeric value representing expansion factor to be applied to generated weights, optional.
-#' @param summary Boolean value for whether to show summary output of the procedure, optional.
+#' @param df Data frame containing data where weights are desired.
+#' @param universe Output object created with \code{universe()} function.
+#' @param wgt.name Name given to column of weights to be added to data, optional.
+#' @param max.wgt Maximum value weights can take on, optional.
+#' @param threshold Value specifying minimum summed difference between weighted 
+#' marginal proportions of sample and universe before algorithm quits, optional.
+#' @param max.iter Value capping number of iterations for the procedure, optional.
+#' @param stuck.limit Value capping the number of times summed differences between 
+#' sample and universe can oscillate between increasing and decreasing, optional.
+#' @param summary Whether or not to display summary output of the procedure, optional.
 #' 
 #' @importFrom dplyr pull enquo mutate select %>%
 #' @importFrom rlang !!
 #' @importFrom data.table data.table setkey
 #' @importFrom crayon red yellow green bold %+%
-#' @importFrom tibble as.tibble
+#' @importFrom tibble as_tibble
 #' @importFrom scales percent
 #' 
 #' @return Data frame with the resulting weight variable appended to it.
 #' 
 #' @examples 
+#' library(iterake)
 #' data(weight_me)
 #' 
 #' iterake(
 #'     df = weight_me,
-#'     design = universe(
+#'     universe = universe(
 #'         df = weight_me,
 #' 
 #'         build_margin(
@@ -50,90 +55,98 @@
 #' )
 #' 
 #' @export
-iterake <- function(df, design, wgt.name = "weight", 
-                    wgt.lim = 3, threshold = 1e-20, max.iter = 50, 
-                    stuck.limit = 5, N, summary = TRUE) {
+iterake <- function(df, universe, wgt.name = "weight", 
+                    max.wgt = 3, threshold = 1e-20, max.iter = 50, 
+                    stuck.limit = 5, summary = TRUE) {
     
     # step 1) setup + error checking ----
-    if (!("universe" %in% class(design))) {
-        stop("Input to `design` must be output created by `universe()`.")
+    if (!("universe" %in% class(universe))) {
+        stop("Input to `universe` must be output created by `universe()`.")
     }
+    
+    # set up useful objects off the bat
     
     # do stuff to to_weight - make data.table and use rn as index
     to_weight <- data.table(df, keep.rownames = TRUE)[, rn := as.numeric(rn)]
+    N <- nrow(to_weight)
+    wgt_cats <- universe[["wgt_cat"]]
     
-    # wgt_cats to get used later
-    wgt_cats <- pull(design, wgt_cat)
-    
-    # make sure dataframe is supplied
+    # make sure data frame is supplied
     if (!is.data.frame(df)) {
         stop("Input to `df` must be a data frame.")
     }
     
     # make sure all wgt_cats are found in data
-    not_in_data <- wgt_cats[!wgt_cats %in% names(df)]
+    wgt_cats_not_in_data <- wgt_cats[!wgt_cats %in% names(df)]
     
-    if (length(not_in_data) > 0) {
+    if (length(wgt_cats_not_in_data) > 0) {
         stop(paste("The following weight category names are not found in your data:",
-                   paste(not_in_data, collapse = ", ")),
+                   paste(wgt_cats_not_in_data, collapse = ", ")),
              sep = "\n")
     }
     
-    # make sure numeric stuff is numeric, character is character, lengths are 1
+    # wgt.name
+    if (any(
+        length(wgt.name) > 1,
+        !is.character(wgt.name)
+    )) {
+        
+        stop("Input to `wgt.name` must be a single character string.")
+    }
     
-    ## wgt.lim
-    if (length(wgt.lim) > 1) {
-        
-        stop("wgt.lim must be a numeric value of length 1.")
-        
-    } else if (!is.numeric(wgt.lim)) {
-        
-        stop("wgt.lim must be numeric.")
-        
-    } else if (wgt.lim <= 1) {
-        
-        stop("wgt.lim must be a numeric value greater than 1.")
+    if (wgt.name %in% names(df)) {
+        stop(paste0("Column name '", wgt.name, "' already exists in the data. Please supply a different name to `wgt.name`."))
+    }
     
-    ## threshold        
-    } else if (length(threshold) > 1) {
+    # max.wgt
+    if (any(
+        length(max.wgt) > 1,
+        !is.numeric(max.wgt),
+        max.wgt <= 1
+    )) {
         
-        stop("threshold must be a numeric value of length 1.") 
-            
-    } else if (!is.numeric(threshold)) {
-        
-        stop("threshold must be numeric.")
-    
-    ## max.iter
-    } else if (length(max.iter) > 1) {
-        
-        stop("max.iter must be a numeric value of length 1.") 
-        
-    } else if (!is.numeric(max.iter)) {
-        
-        stop("max.iter must be numeric.")
-        
-    } else if (max.iter <= 0) {
-        
-        stop("max.iter must be a numeric value greater than 0.")
-    
-    ## wgt.name    
-    } else if (!is.character(wgt.name)) {
-        
-        warning(paste0("coercing wgt.name '", wgt.name, "' to character."))
-        wgt.name <- as.character(wgt.name)
-        
-    } else if (length(wgt.name) > 1) {
-        
-        stop("wgt.name must be a character string of length 1.")
+        stop("Input to `max.wgt` must be a single numeric value greater than 1.")
         
     }
     
-    # N for expansion factor
-    if (!missing(N)) {
-        if (!is.numeric(N) || length(N) != 1) {
-            stop("`N` must be a numeric value of length 1 corresponding to size of population.")
-        }
+    # threshold
+    if (any(
+        length(threshold) > 1,
+        !is.numeric(threshold),
+        threshold < 0
+    )) {
+        
+        stop("Input to `threshold` must be a single positive numeric value.")
     }
+    
+    # max.iter
+    if (any(
+        length(max.iter) > 1,
+        !is.numeric(max.iter),
+        max.iter <= 0
+    )) {
+        
+        stop("Input to `max.iter` must be a single numeric value greater than 0.")
+        
+    }
+    
+    # stuck.limit
+    if (any(
+        length(stuck.limit) > 1,
+        !is.numeric(stuck.limit),
+        stuck.limit < 1
+    )) {
+        
+        stop("Input to `stuck.limit` must be a single numeric value greater than or equal to 1.")
+        
+    }
+    
+    # summary
+    if (!is.logical(summary)) {
+        stop("Input to `summary` requires TRUE/FALSE.")
+    }
+    
+
 
     # trim things and initialize wgt = 1
     to_weight <- to_weight[, 
@@ -155,9 +168,6 @@ iterake <- function(df, design, wgt.name = "weight",
     stuck_count <- 0
     stuck_check <- 0
     
-    N <- nrow(to_weight)
-    wgt_names <- design[["wgt_cat"]]
-    
     # do the loops until the threshold is reached
     while (check > threshold) {
 
@@ -173,14 +183,14 @@ iterake <- function(df, design, wgt.name = "weight",
         # reset/initialize check value
         check <- 0
         
-        # loop through each variable in design$wgt_cat to generate weight
-        for (i in seq_along(wgt_names)) {
+        # loop through each variable in universe$wgt_cat to generate weight
+        for (i in seq_along(wgt_cats)) {
             
             # data.table versions of:
             # - data being weighted, i'th weighting column as key
-            # - i'th dataset (targets) of design
-            DT_data   <- data.table(to_weight, key = wgt_names[i])
-            DT_design <- data.table(design[["data"]][[i]])
+            # - i'th dataset (targets) of universe
+            DT_data   <- data.table(to_weight, key = wgt_cats[i])
+            DT_design <- data.table(universe[["data"]][[i]])
             
             # start with original data.table-ized object
             DT_merge <- 
@@ -193,7 +203,7 @@ iterake <- function(df, design, wgt.name = "weight",
                     DT_data[, 
                             # calcs the "haves" by the i'th weighting category
                             .(act_prop = sum(wgt) / N),
-                            by = c(wgt_names[i])
+                            by = c(wgt_cats[i])
                             
                             # merge haves with wants
                             ][DT_design
@@ -201,27 +211,27 @@ iterake <- function(df, design, wgt.name = "weight",
                               ][, 
                                 # calculate wants over haves by the i'th weighting category
                                 .(wgt_temp = ifelse(act_prop == 0, 0, targ_prop / act_prop)), 
-                                by = c(wgt_names[i])
+                                by = c(wgt_cats[i])
                                 ]
                     ]
             
             # creates the weight factor, max out at the weight limit
-            to_weight <- DT_merge[, wgt := ifelse(wgt * wgt_temp > wgt.lim, wgt.lim, wgt * wgt_temp)][
+            to_weight <- DT_merge[, wgt := ifelse(wgt * wgt_temp > max.wgt, max.wgt, wgt * wgt_temp)][
                 # temporary weight no longer needed
                 , wgt_temp := NULL]
             
         }
 
         # loop through each to calculate discrepencies
-        for (i in seq_along(wgt_names)) {
+        for (i in seq_along(wgt_cats)) {
             
-            DT_design <- data.table(design[["data"]][[i]])
-            DT_data   <- data.table(to_weight, key = wgt_names[i])
+            DT_design <- data.table(universe[["data"]][[i]])
+            DT_data   <- data.table(to_weight, key = wgt_cats[i])
             
             sum_diffs <- DT_data[, 
                                    # calcs the "haves" by the i'th weighting category
                                    .(act_prop = sum(wgt) / N),
-                                   by = c(wgt_names[i])
+                                   by = c(wgt_cats[i])
                                    
                                    # merge haves with wants
                                    ][DT_design
@@ -261,7 +271,7 @@ iterake <- function(df, design, wgt.name = "weight",
         out_bad <- red $ bold
         out <- NULL
         
-        title1 <- 'iterake summary & effects'
+        title1 <- 'iterake summary'
         num_dashes <- nchar(title1) + 4
         rem_dashes <- 80 - num_dashes
         
@@ -304,7 +314,7 @@ iterake <- function(df, design, wgt.name = "weight",
                         # scrap no-longer-needed rn
                         , rn := NULL] %>%
 
-            as.tibble()
+            as_tibble()
 
         # calculate stats
         wgt <- out$wgt
@@ -324,7 +334,7 @@ iterake <- function(df, design, wgt.name = "weight",
         rem_dashes <- 80 - num_dashes
         
         # return output message?
-        if (isTRUE(summary)) {
+        if (summary) {
             
             cat('\n-- ' %+% 
                     bold(title1) %+% 
@@ -332,7 +342,7 @@ iterake <- function(df, design, wgt.name = "weight",
                     paste(rep('-', times = rem_dashes), collapse = "") %+%
                     '\n')
             if (stuck_check > 0) {
-                cat(' Convergence: ' %+% yellow('Success '%+% '\U2714') %+% '\n')
+                cat(' Convergence: ' %+% green('Success '%+% '\U2714') %+% '\n')
             } else {
                 cat(' Convergence: ' %+% green('Success '%+% '\U2714') %+% '\n')    
             }
@@ -345,12 +355,12 @@ iterake <- function(df, design, wgt.name = "weight",
             
             if (stuck_check > 0) {
                 cat(' NOTE: ' %+% 
-                        yellow('Iterations stopped at a difference of ' %+% 
+                        paste0('Threshold met, stopped at difference of ' %+% 
                                    paste0(
                                        formatC(stuck_check, 
                                                format = "e", 
                                                digits = 3))) %+% 
-                        '\n\n')
+                        ' between weighted sample and universe.\n\n')
             }
         }
         
