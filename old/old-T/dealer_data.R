80b5225af9b877b251152b1364742913923e3a6e
ttrodrigz <ttrodrigz@gmail.com>
2019-09-19 17:37:17 -0400

    Consolidated /old, edit to README.

---
+++ old/old-T/dealer_data.R
@@ -0,0 +1,19 @@
+#' Simulated car dealership data
+#'
+#' A sample of 400 observations from a customer satisfaction survey.
+#'
+#' @docType data
+#'
+#' @usage data(dealer_data)
+#'
+#' @format A data frame with 200 rows and six columns:
+#' \describe{
+#'    \item{Sales_ID}{unique sales record}
+#'    \item{Age}{age group of customer}
+#'    \item{Year}{year in which the sale occurred}
+#'    \item{Type}{body style of the vehicle purchased}
+#' }
+#'
+#' @examples
+#' data(dealer_data)
+"dealer_data"
