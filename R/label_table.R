36acae171872c05ac461e80fcac68ca23dc4ce7f
dwitherell <devonwitherell@hotmail.com>
2019-09-27 06:39:06 -0600

    Updated label_table based on toolbox code; incremented version

--- R/label_table.R
+++ R/label_table.R
@@ -1,183 +1,223 @@
-#' Create table of variable or value labels
+#' Create Data Frame of Variable or Value Labels.
 #'
-#' It is often useful to inspect variable or value labels attached to datasets
-#' (like with SAV files from SPSS). This function will return a tibble of variables and
-#' their associated labels, or, if requested, a variable and its value labels.
+#' This enables you to inspect labels at the variable or value label.
+#' If requesting value labels for a \code{var}, the default is to also
+#' tabulate the frequencies and percents. Can provide an optional column
+#' of weights to calculated weighted frequencies and percentages.
 #'
-#' @param data Data frame.
-#' @param var A variable in the dataset where labels are desired.
-#' @param freq Boolean, indicates whether frequencies should be included, optional.
-#' @param weight A variable in the dataset representing weights to be used 
-#' for optional frequency calculation, optional.
-#' 
-#' @importFrom labelled remove_val_labels val_labels is.labelled
-#' @importFrom Hmisc contents
-#' @importFrom tibble tibble enframe
-#' @importFrom tidyr drop_na
-#' @importFrom purrr map_df set_names
-#' @importFrom dplyr filter mutate enquo quo_name pull left_join select bind_rows arrange group_by summarise ungroup
-#' @importFrom rlang !! sym :=
+#' @param data A data frame where variable or value labels are requested.
+#' @param var If value labels are requested, provide corresponding column (unquoted) in the data frame.
+#' @param freq Logical, whether or not to tabulate frequencies for the value labels.
+#' @param weight An unquoted column of weights.
 #'
-#' @return By default, will return a dataset of variables and labels of every
-#' variable in the dataset. If specified, returns a table of values in a
-#' variable and its labels, as well as frequencies.
+#' @return A data frame of variable or value labels.
 #'
 #' @examples
-#' data(demo_data)
-#' 
-#' label_table(data = demo_data, var = EyeColor)
+#' library(dplyr)
+#' library(labelled)
+#' library(iterake)
+#'
+#' df <-
+#'   data.frame(
+#'     x = sample(c(1, 2), 100, replace = TRUE),
+#'     w = rnorm(100, 1, 0.1)
+#'   ) %>%
+#'   set_variable_labels(
+#'     x = "Gender",
+#'     w = "Weight"
+#'   ) %>%
+#'   set_value_labels(
+#'     x = c("Male" = 1, "Female" = 2)
+#'   )
+#'
+#' label_table(df)
+#' label_table(df, x, freq = FALSE)
+#' label_table(df, x)
+#' label_table(df, x, weight = w)
+#'
+#' @importFrom glue glue
+#' @importFrom tidyr drop_na
+#' @importFrom purrr set_names
+#' @importFrom labelled var_label val_labels remove_val_labels
+#' @importFrom tibble enframe tibble
+#' @importFrom dplyr %>% enquo quo_name pull select bind_rows arrange left_join
+#' mutate group_by summarise ungroup filter
+#' @importFrom rlang := sym
+#' @importFrom crayon green red
 #'
 #' @export
-label_table <- function(data, var, freq = FALSE, weight) {
-
-    # when only variable labels are requested ----
+label_table <- function(data, var, freq = TRUE, weight) {
+    
+    # create variable label table ---------------------------------------------
+    
+    # `labelled` can be used to extract variable labels
+    # replacing NULL's with NA avoids dropping vars with unlist()
     if (missing(var)) {
-
-        # this stops a "1" from being printed where value labels
-        # exist on a variable with no variable labels
-        # this is an artifact of the contents method of Hmisc
-        data <- remove_val_labels(data)
-
-        # do any of the variables have labels?
-        var_labels <- contents(data)$contents$Labels
-
-        if (is.null(var_labels)) {
-            stop("Data provided contains no variable labels, did you mean to request value labels for a specific variable?")
+        
+        var.tbl <- var_label(data)
+        var.tbl[sapply(var.tbl, is.null)] <- NA
+        var.tbl <- unlist(var.tbl) %>% enframe("var", "lab")
+        return(var.tbl)
+        
+    }
+    
+    # create value label table ------------------------------------------------
+    # crappy check for string - if it is, as.symbol
+    check <- try(is.character(var), silent = TRUE)
+    
+    if (!"try-error" %in% class(check)) {
+        if (check) {
+            var <- sym(var)
         }
-
-        # build the table
-        out <- tibble(
-            var = names(data),
-            lab = var_labels
+    }
+    
+    # set up for tidy eval
+    var.name      <- enquo(var)
+    var.name.chr  <- quo_name(var.name)
+    var.label.chr <- paste(var.name.chr, "lab", sep = "_")
+    var.label.sym <- sym(var.label.chr)
+    
+    # extract value labels and early return if NULL
+    val.labs <-
+        data %>%
+        pull({{ var.name }}) %>%
+        val_labels()
+    
+    if (is.null(val.labs)) {
+        warning(glue("Value labels do not exist for variable `{var.name.chr}`."))
+        return(NULL)
+    }
+    
+    val.tbl <-
+        val.labs %>%
+        enframe(
+            name = var.label.chr,
+            value = var.name.chr
         ) %>%
-            # both names and labels should be characters
-            map_df(as.character) %>%
-            # makes more sense to NA the empty strings
-            mutate(lab = ifelse(lab == "", NA, lab))
-
-        return(out)
-
+        select(2, 1)
+    
+    # NA values get dropped by `val_labels()`, need to add those values back in
+    in.data   <- data %>% pull({{ var.name }}) %>% unique() %>% sort(na.last = TRUE)
+    in.labels <- val.tbl %>% pull(1) %>% unique() %>% sort(na.last = TRUE)
+    val.need  <- in.data[! in.data %in% in.labels]
+    
+    # if there's nothing else to add, just return what we have
+    if (!length(val.need) == 0) {
+        
+        # this gets binded to the existing label table
+        need.tbl <- tibble({{ var.name.chr }} := val.need,
+                           {{ var.label.chr }} := NA)
+        
+        val.tbl <-
+            val.tbl %>%
+            bind_rows(need.tbl) %>%
+            arrange({{ var.name.sym }})
     }
-
-    # when value labels are requested ----
-    if (!missing(var)) {
-
-        var_name      <- enquo(var)
-        var_name_chr  <- quo_name(var_name)
-        var_label_chr <- paste0(quo_name(var_name), "_lab")
-
-        # does this variable have value lables?
-        if (!is.labelled(data %>% pull(!! var_name))) {
-            warning(paste0(
-                "Value labels do not exist for `",
-                quo_name(var_name),
-                "`."))
-
-            return(NULL)
-        }
-
-        # create a table of the variable levels and their labels
-        label_table <-
-            data %>%
-            pull(!! var_name) %>%
-            val_labels() %>%
-            enframe(
-                name = var_label_chr,
-                value = var_name_chr
-            ) %>%
-            select(2, 1)
-
-        # need to add in extra levels in the data that aren't accounted
-        # for by the label attributes
-
-        in.data   <- data %>% pull(!! var_name) %>% unique() %>% sort(na.last = TRUE)
-        in.labels <- label_table %>% pull(1) %>% unique() %>% sort(na.last = TRUE)
-
-        needed <- in.data[!in.data %in% in.labels]
-
-        # if there's nothing else to add, just return what we have
-        if (!length(needed) == 0) {
-
-            # this gets binded to the existing label table
-            need_table <- tibble(!! var_name_chr := needed,
-                                 !! var_label_chr := NA)
-
-            label_table <-
-                label_table %>%
-                # bind what's needed and sort
-                bind_rows(need_table) %>%
-                arrange(!! sym(var_name_chr))
-        }
-
-        label_table
-    }
-
+    
+    
+    # if freqs are requested --------------------------------------------------
+    
     if (freq) {
-
+        
         N <- nrow(data)
-        N_no_miss <- data %>% select(!! var_name) %>% drop_na() %>% nrow()
-
+        N.valid <- data %>% select({{ var.name }}) %>% drop_na() %>% nrow()
+        
         if (missing(weight)) {
-
-            freq_df <-
+            
+            freq.tbl <-
                 data %>%
-                select(!! var_name) %>%
+                select({{ var.name }}) %>%
                 mutate(weight = 1) %>%
-                set_names(var_name_chr, "weight") %>%
+                set_names(var.name.chr, "weight") %>%
                 remove_val_labels()
-
+            
         }
-
-        if (!missing(weight)) {
-            weight_name <- enquo(weight)
-
-            freq_df <-
+        
+        if (! missing(weight)) {
+            
+            weight.name <- enquo(weight)
+            weight.name.chr <- quo_name(weight.name)
+            weight.is.numeric <- is.numeric(data[[weight.name.chr]])
+            
+            freq.tbl <-
                 data %>%
-                select(!! var_name, !! weight_name) %>%
-                set_names(var_name_chr, "weight") %>%
+                select({{ var.name }}, {{ weight.name }}) %>%
+                set_names(var.name.chr, "weight") %>%
                 remove_val_labels()
-
+            
+            # putting this check after the data wrangling, will error out
+            # due to not finding the column before moving on to the class check
+            
+            if (! weight.is.numeric) {
+                stop("Input to `weight` must be numeric.")
+            }
+            
         }
-
+        
         # calc freq and percent
-        pre_valid <-
-            freq_df %>%
-            group_by(!! var_name) %>%
+        before.valid.tbl <-
+            freq.tbl %>%
+            group_by({{ var.name }}) %>%
             summarise(freq = sum(weight)) %>%
             ungroup() %>%
             mutate(pct = freq / N)
-
+        
         # calc valid percent
-        with_valid <-
-            pre_valid %>%
-            filter(!is.na(!! var_name)) %>%
-            mutate(valid.pct = freq / N_no_miss) %>%
-            select(!! var_name, valid.pct)
-
-        if (missing(weight)) {
-            cat("Unweighted\n")
-        } else {
-            cat("Weighted\n")
-        }
-
-        # getting rid of warnings, joining LHS and RHS thing
+        with.valid.tbl <-
+            before.valid.tbl %>%
+            filter(!is.na({{ var.name }})) %>%
+            mutate(valid.pct = freq / N.valid) %>%
+            select({{ var.name }}, valid.pct)
+        
+        # warnings due to attribute labels differeing on LHS and RHS of join
         suppressWarnings(
-            out <-
-                # join all components
-                label_table %>%
+            val.tbl <-
+                val.tbl %>%
                 remove_val_labels() %>%
-                left_join(pre_valid, by = var_name_chr) %>%
-                left_join(with_valid, by = var_name_chr)
-
+                left_join(before.valid.tbl, by = var.name.chr) %>%
+                left_join(with.valid.tbl, by = var.name.chr)
         )
-
-        return(out)
-
     }
-
-    out <- label_table
-    out
-
+    
+    if (! missing(var)) {
+        
+        var.text <-
+            data %>%
+            select({{ var.name }}) %>%
+            var_label() %>%
+            unlist()
+        
+        if(freq) {
+            if (!missing(weight)) {
+                cat(glue(
+                    "Var: {var.name.chr}",
+                    "\n",
+                    "Lab: {var.text}",
+                    "\n",
+                    "Tab: {green('Weighted')}",
+                    "\n\n"
+                ))
+            } else {
+                cat(glue(
+                    "Var: {var.name.chr}",
+                    "\n",
+                    "Lab: {var.text}",
+                    "\n",
+                    "Tab: {red('Unweighted')}",
+                    "\n\n"
+                ))
+            }
+        } else {
+            cat(glue(
+                "Var: {var.name.chr}",
+                "\n",
+                "Lab: {var.text}",
+                "\n\n"
+            ))
+        }
+    }
+    
+    return(val.tbl)
+    
 }
-
-utils::globalVariables(c("lab", "valid.pct", "wgt_temp", "prop_diff", "rn"))
+globalVariables(c("valid.pct", "var.name.sym"))
