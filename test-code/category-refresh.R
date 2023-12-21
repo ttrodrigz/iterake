b917ef10673320022ec6cec276c40cb0c318395b
Tony Rodriguez <ttrodrigz@gmail.com>
2023-12-21 11:04:11 -0500

    More new iterake stuff

---
+++ test-code/category-refresh.R
@@ -0,0 +1,105 @@
+library(labelled)
+library(tidyverse)
+library(rlang)
+library(collapse)
+library(glue)
+library(cli)
+
+category <- function(name, buckets, targets) {
+    
+    # Check to make sure buckets and targets are the same length
+    if (length(buckets) != length(targets)) {
+        abort("The number of buckets must equal the number of targets.")
+    }
+    
+    # Store as a list
+    out <- list(list(
+        buckets = buckets,
+        targets = targets
+    ))
+    
+    names(out) <- name
+    
+    # Check to make sure that the targets sum to 1
+    sum.targets <- fsum(targets)
+    sum1 <- sum.targets == 1
+    
+    if (!sum1) {
+        warn(glue(
+            "Weighting targets for '{name}' will be re-proportioned to sum to 1."
+        ))
+        targets.sum1 <- targets / sum.targets
+        out[[1]]$targets_sum1 <- targets.sum1
+    }
+    
+    # Are the buckets labelled?
+    is.labelled <- labelled::is.labelled(buckets)
+    
+    if (is.labelled) {
+        out[[1]]$labels <- names(labelled::val_labels(buckets))
+    }
+    
+    class(out) <- c(class(out), "category")
+    
+    out
+    
+}
+
+aa <- category(
+    name = "color",
+    buckets = c("blue", "red", "green"),
+    targets = c(1, 2, 3)
+)
+
+bb <- category(
+    name = "this_that",
+    buckets = labelled_spss(
+        x = 1:3,
+        labels = c("One" = 1, "Two" = 2, "Three" = 3)
+    ),
+    targets = c(2.3, 1.1, 2.9)
+)
+
+#' @method print category
+#' @export
+print.category <- function(x, digits = 3) {
+    
+    # Was this weight category re-proportioned?
+    was.reprop <- !is.null(x[[1]]$targets_sum1)
+    
+    # What's the weight category name?
+    wgt.cat <- names(x)
+    
+    # What are the buckets and the targets?
+    buckets <- x[[1]][["buckets"]]
+    targets <- x[[1]][["targets"]]
+    targets_sum1 <- x[[1]][["targets_sum1"]]
+    
+    # Header line
+    cli::cat_rule(glue("Category: {wgt.cat}"))
+    
+    # Build the bullet point strings
+    is.labelled <- labelled::is.labelled(buckets)
+    if (is.labelled) {
+        labels <- x[[1]]$labels
+        buckets <- glue("{buckets} [{labels}]")
+    }
+    buckets <- paste0(buckets, ":")
+    bucket.chr <- nchar(buckets)
+    max.chr <- max(bucket.chr)
+    
+    buckets <- str_pad(buckets, width = max.chr, side = "right", pad = ' ')
+    
+    if (was.reprop) {
+        cli::cat_line(cli::style_italic("Targets were re-proportioned to sum to 1"))
+        values <- scales::number(targets_sum1, accuracy = 1/10^digits, trim = FALSE)
+    } else {
+        values <- scales::number(targets, accuracy = 1/10^digits, trim = FALSE)
+    }
+    
+    cli::cat_line(glue("{buckets} {values}"))
+    
+}
+
+print(aa, digits = 4)
+›
--- test-code/iterake-refresh.R
+++ test-code/iterake-refresh.R
@@ -5,24 +5,24 @@
 # version of the iterake function.
 
 # This does not do any error checking or exception handling and does not
-# incoporate the permutation version. But, I think it works!
+# incorporate the permutation version. But, I think it works!
 
 # Packages ----------------------------------------------------------------
 
 library(tidyverse)
 library(collapse)
+library(cli)
 
 
 # Create some data --------------------------------------------------------
 
-ss <- 20
+ss <- 20000
 
 set.seed(101)
 tst <- tibble(
     x = factor(sample(letters[1:4], size = ss, replace = TRUE)),
     y = factor(sample(letters[5:7], size = ss, replace = TRUE)),
-    z = factor(sample(letters[8:10], size = ss, replace = TRUE)),
-    wgt = rep(1, times = ss)
+    z = factor(sample(letters[8:10], size = ss, replace = TRUE))
 )
 
 
@@ -34,90 +34,65 @@ want <- list(
     z = c("h" = 0.25, "i" = 0.44, "j" = 0.31)
 )
 
+want.tibbles <- want
 
-# Initial values ----------------------------------------------------------
-
-tmp <- tst
-threshold <- 0.0000001
-cap <- 3
-counter <- 0
-
-wt.cats <- names(want)
-
-have <- 
-    tst |> 
-    fselect(wt.cats) |> 
-    lapply(qtab, w = tst$wgt, dnn = NULL) |> 
-    lapply(prop.table)
+for (i in seq_along(want)) {
+    
+    want.tibbles[[i]] <- enframe(
+        x = want.tibbles[[i]],
+        name = names(want.tibbles[i]),
+        value = "want"
+    )
+    
+}
 
 
-delta <- sum(map2_dbl(
-    .x = want,
-    .y = have,
-    .f = \(w, h) sum(abs(w - h))
-))
-
+control_iterake <- function(
+        threshold = 1e-10,
+        max_weight = 3,
+        max_iter = 50,
+        max_stuck = 5
+) {
+    list(
+        threshold = threshold,
+        max_weight = max_weight,
+        max_iter = max_iter,
+        max_stuck = max_stuck
+    )
+}
 
 # Do stuff ----------------------------------------------------------------
 
-while (delta >= threshold) {
+iterake <- function(
+        x,
+        targets,
+        control = control_iterake()
+) {
     
-    counter <- counter + 1
+    # Initialize things
+    tmp <- fmutate(x, ...wgt... = 1)
+    rep.counter   <- 0
+    stuck.counter <- 0
+    delta.log     <- numeric()
+    wt.cats       <- names(targets)
     
-    # Create the weights
-    for (i in seq_along(wt.cats)) {
-        
-        cat.now <- wt.cats[[i]]
-        
-        # tibble up the wants - eventually this should happen outside
-        # of the loop at the initial phases of the function
-        want.df <- enframe(
-            x = want[[cat.now]],
-            name = cat.now,
-            value = "want"
-        )
-        
-        tmp <-
-            
-            tmp |> 
-            
-            # sum of the weights by group to get the "haves"
-            # `fsum()` is faster than `sum()` especially when used
-            # in the context of `fgroup_by()` and `fmutate()`
-            fgroup_by(cat.now) |> 
-            fmutate(have = fsum(wgt) / ss) |> 
-            fungroup() |> 
-            
-            # join in the "wants"
-            join(
-                y = want.df,
-                on = cat.now,
-                verbose = FALSE
-            ) |> 
-            
-            # weight factor is the "wants" / "haves"
-            # w / h will result in Inf if the denominator is 0
-            # `replace_inf()` is faster and uses less memory than
-            # using an `ifelse()`
-            fmutate(wgt_fct = replace_inf(want / have, value = 0)) |> 
-            
-            # create the new weight by multiplying the old by the
-            # newly created weighting factor
-            # `pmin()` faster and uses less memory than `ifelse()`
-            fmutate(wgt = pmin(cap, wgt * wgt_fct)) |> 
-            
-            # grab columns needed
-            fselect(wt.cats, "wgt")
-        
-    }
+    # Stuff from the control
+    threshold  <- control$threshold
+    max.weight <- control$max_weight
+    max.iter   <- control$max.iter
+    max.stuck  <- control$max_stuck
     
-    # Check the deltas
+    # Unpack the wants for faster calculation of the deltas
+    want <- map(targets, \(x) pull(x, want, name = 1))
+    
+    # Calculate the initial "haves"
     have <- 
         tmp |> 
         fselect(wt.cats) |> 
-        lapply(qtab, w = tmp$wgt, dnn = NULL) |> 
+        lapply(qtab, w = tmp[["...wgt..."]], dnn = NULL) |> 
         lapply(prop.table)
     
+    # Calculate the sum of the absolute difference of the "wants" and "haves"
     delta <- sum(map2_dbl(
         .x = want,
         .y = have,
@@ -125,11 +100,156 @@ while (delta >= threshold) {
     ))
     
     
+    while (delta >= threshold) {
+        
+        # Increment the counter
+        rep.counter <- rep.counter + 1
+        
+        # Create the weights
+        for (i in seq_along(wt.cats)) {
+            
+            cat.now <- wt.cats[[i]]
+            
+            want.now <- want.tibbles[[cat.now]]
+            
+            tmp <-
+                
+                tmp |> 
+                
+                # sum of the weights by group to get the "haves"
+                # `fsum()` is faster than `sum()` especially when used
+                # in the context of `fgroup_by()` and `fmutate()`
+                fgroup_by(cat.now) |> 
+                fmutate(have = fsum(...wgt...) / ss) |> 
+                fungroup() |> 
+                
+                # join in the "wants"
+                join(
+                    y = want.now,
+                    on = cat.now,
+                    verbose = FALSE
+                ) |> 
+                
+                # weight factor is the "wants" / "haves"
+                # w / h will result in Inf if the denominator is 0
+                # `replace_inf()` is faster and uses less memory than
+                # using an `ifelse()`
+                fmutate(wgt_fct = replace_inf(want / have, value = 0)) |> 
+                
+                # create the new weight by multiplying the old by the
+                # newly created weighting factor
+                # `pmin()` faster and uses less memory than `ifelse()`
+                fmutate(...wgt... = pmin(max.weight, ...wgt... * wgt_fct)) |> 
+                
+                # grab columns needed
+                fselect(wt.cats, "...wgt...")
+            
+        }
+        
+        # Re-calculate the "haves"
+        have <- 
+            tmp |> 
+            fselect(wt.cats) |> 
+            lapply(qtab, w = tmp[["...wgt..."]], dnn = NULL) |> 
+            lapply(prop.table)
+        
+        # Calculate the sum of the absolute difference of the "wants" and "haves"
+        delta <- sum(map2_dbl(
+            .x = want,
+            .y = have,
+            .f = \(w, h) sum(abs(w - h))
+        ))
+        
+        
+        # In some situations the algorithm will get stuck where the deltas will
+        # actually increase from the prior iteration and will fluctuate back and
+        # forth. This keeps a counter of that issue and will break the loop if it
+        # reaches the specified limit.
+        
+        if (rep.counter > 1) {
+            
+            if (delta > tail(delta.log, 1)) {
+                
+                stuck.counter <- stuck.counter + 1
+                
+                if (stuck.counter == control$max_stuck) {
+                    break
+                }
+            }
+        }
+        
+        
+        # Keep a log of the deltas.
+        delta.log <- c(delta.log, delta)
+        
+        
+        if (rep.counter == control$max_iter) {
+            break
+        }
+        
+        
+    }
     
-}
+    res <- pull(tmp, ...wgt...)
+    
+    
+    cat_line("Delta")
+    print(delta)
 
-# Show results
-want
-have
-delta
-counter
+    cat_line("Counter")
+    print(rep.counter)
+
+    cat_line("Stuck Counter")
+    print(stuck.counter)
+
+    cat_line("Delta Log")
+    print(delta.log)
+
+    cat_line("Weights")
+    print(res)
+
+    list(
+        "delta" = delta.log,
+        "counter" = rep.counter,
+        "stuck_counter" = stuck.counter,
+        "results" = res
+    )
+} 
+
+iterake(
+    x = tst,
+    targets = want.tibbles
+)
+
+
+
+# Ideas -------------------------------------------------------------------
+
+#' Here are ideas for functions to use downstream of iterake(), all of 
+#' these will use the output of iterake() as its primary input with some
+#' additional parameters.
+#' 
+#' 1. append_weights(x, weight_name = "weight"): creates a tibble of the
+#'    original data plus the column of weights appended with its specified
+#'    name.
+#' 2. extract_weights(x, weight_name = "weight"): creates a 1-column tibble
+#'    of just the created weights with the specified weight name.
+#' 3. weight_stats(x): creates a 1-row tibble of the summary statistics of
+#'    the created weights...
+#'    - Unweighted base
+#'    - Weighted base
+#'    - Effective base
+#'    - Quantiles, mean, standard deviation of the weights
+#' 4. iterake_stats(x): creates a 1-row tibble of the statistics of the
+#'    algorithm such as number of iterations, final convergence, number of
+#'    time stuck, time it took, etc.
+#' 5. summary(x, digits = 3): use this generic to nicely print out what weight_stats()
+#'    and iterake_stats() generates.
+#' 6. print(x, digits = 3): use this generic to print a simple print message of what
+#'    took place.
+#' 7. autoplot(x, what = c("hist", "compare"), binwidth = 0.1): use this 
+#'    generic from ggplot2 to plot the histogram of the weights ("hist"), 
+#'    or the weighted and unweighted props compared to the targets 
+#'    for "compare". Note: there can/should also be an autoplot() function
+#'    for the output of universe() that will plot the actual vs targets but
+#'    it will just be unweighted.
