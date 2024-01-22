e949662d8c043a9d37832af6556c65f1b0803e10
Tony Rodriguez <ttrodrigz@gmail.com>
2024-01-22 09:36:00 -0500

    Added weighting calc stats

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,4 @@
-#' Type compatability check
+#' Type compatibility check
 #' 
 #' Internal function to verify match between category group vector type and 
 #' data vector type.
@@ -34,4 +34,84 @@ compatible_types <- function(x, y) {
     
     TRUE
     
+}
+
+
+#' Unweighted sample size.
+#' 
+#' @param x A vector.
+#' 
+#' @importFrom collapse fsum
+#' 
+#' @examples
+#' unweighted_ss(
+#'     x = runif(3),
+#' )
+#' 
+#' @export
+unweighted_ss <- function(x) {
+    
+    fsum(!is.na(x))
+    
+}
+
+#' Weighted sample size.
+#' 
+#' @param x A vector.
+#' @param w A vector of weights.
+#' 
+#' @importFrom collapse fsum
+#' 
+#' @examples
+#' weighted_ss(
+#'     x = runif(3),
+#'     w = c(0.75, 1.00, 1.25)
+#' )
+#' 
+#' @export
+weighted_ss <- function(x, w) {
+    
+    valid <- complete.cases(cbind(x, w))
+    
+    fsum(w[valid])
+    
+}
+
+#' Effective sample size.
+#' 
+#' @param x A vector.
+#' @param w A vector of weights.
+#' 
+#' @importFrom collapse fsum
+#' 
+#' @examples
+#' effective_ss(
+#'     x = runif(3),
+#'     w = c(0.75, 1.00, 1.25)
+#' )
+#' 
+#' @export
+effective_ss <- function(x, w) {
+    
+    valid <- complete.cases(cbind(x, w))
+    
+    w <- w[valid]
+    
+    (sum(w) ^ 2) / sum(w ^ 2)
+    
+}
+
+
+#' Weighting efficiency.
+#' 
+#' @param w A vector of weights.
+#' 
+#' @importFrom collapse fsum
+#' 
+#' @examples
+#' weighting_efficiency(c(0.75, 1.00, 1.25))
+#' 
+#' @export
+weighting_efficiency <- function(w) {
+    effective_ss(w, w) / unweighted_ss(w)
 }
